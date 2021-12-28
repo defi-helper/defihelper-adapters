2d1c5b8e35cae8ec287ce35be8aa3e23af90793f
artur <amamedbekov@tokenomica.com>
2021-12-28 20:58:19 +0300

    fix

--- adapters/moonriverHuckleberry/index.js
+++ adapters/moonriverHuckleberry/index.js
@@ -484,7 +484,7 @@ module.exports = {
         ]);
         const rewardToken = new ethers.Contract(rewardTokenAddress, ethereum.abi.ERC20ABI, provider);
         const rewardTokenBalance = await rewardToken.balanceOf(contractAddress).then((v) => v.toString());
-        const pendingReward = await staking.pendingReward(pool.index, walletAddress).then((v) => v.toString());
+        const pendingReward = await staking.pendingReward(pool.index, signerAddress).then((v) => v.toString());
 
         const earned = new bn(pendingReward).plus(rewardTokenBalance);
         if (earned.toString(10) === '0') return new Error('No earned');
