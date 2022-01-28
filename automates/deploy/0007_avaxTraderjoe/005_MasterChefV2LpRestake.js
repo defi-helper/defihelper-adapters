700f29468cd9db699927c87190b2be11abf82527
artur <amamedbekov@tokenomica.com>
2022-01-28 19:12:46 +0300

    fix

--- automates/deploy/0007_avaxTraderjoe/005_MasterChefV2LpRestake.js
+++ automates/deploy/0007_avaxTraderjoe/005_MasterChefV2LpRestake.js
@@ -4,7 +4,7 @@ module.exports = migration(async ({ utils: { dfhNetwork, deploy } }) => {
   const { ERC1167, Storage } = dfhNetwork();
 
   await deploy('avaxTraderjoeMasterChefV2LpRestake', {
-    contract: 'automates/avaxTraderjoe/MasterChefV2SingleRestake.automate.sol:MasterChefV2SingleRestake',
+    contract: 'automates/avaxTraderjoe/MasterChefV2LpRestake.automate.sol:MasterChefV2LpRestake',
     args: [Storage.address],
     libraries: {
       ERC1167: ERC1167.address,
