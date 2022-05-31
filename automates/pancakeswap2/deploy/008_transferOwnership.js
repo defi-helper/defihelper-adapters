da998e87d621b206975fe8242cc7f7f9fcbcc3b3
artur <amamedbekov@tokenomica.com>
2022-05-31 14:05:47 +0300

    pancakeswap masterchef 2 deploy scripts

---
+++ automates/pancakeswap2/deploy/008_transferOwnership.js
@@ -0,0 +1,12 @@
+const { migration } = require('./utils');
+
+module.exports = migration(async ({ utils: { dfhNetwork, read, execute } }) => {
+  const { GovernorMultisig } = dfhNetwork();
+
+  if ((await read('pancakeswapMasterChef2SingleRestake', {}, 'owner')) === GovernorMultisig.address) {
+    return;
+  }
+
+  await execute('pancakeswapMasterChef2SingleRestake', {}, 'transferOwnership', GovernorMultisig.address);
+});
+module.exports.tags = ['GovernanceOwner', 'PancakeSwap'];
