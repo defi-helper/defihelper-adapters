d81eaf3efd438b755f9818a885d93c1a5e32eb73
Luka Nikitin <lukentuichennel@gmail.com>
2022-08-24 13:47:15 +0200

    fix

--- adapters-ts/utils/coingecko.ts
+++ adapters-ts/utils/coingecko.ts
@@ -32,40 +32,24 @@ const errorHandler = (e: AxiosError) => {
 };
 
 export class UniswapV2RouterProvider {
-  static routingMap = {
-    1: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
-    10: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
-    25: "0xcd7d16fB918511BF7269eC4f48d61D79Fb26f918",
-    56: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
-    137: "0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32",
-    250: "0xF491e7B69E4244ad4002BC14e878a34207E38c29",
-    42161: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",
-    1313161554: "0xA1B1742e9c32C7cAa9726d8204bD5715e3419861",
-  };
-
   constructor(
     public readonly provider: ethersType.ethers.providers.Provider,
   ) {}
 
-  async price(route: string[]) {
-    const currentNetwork = await this.provider.getNetwork()
-    const routerAddress = UniswapV2RouterProvider.routingMap[currentNetwork.chainId as keyof typeof UniswapV2RouterProvider.routingMap]
-    if(!routerAddress) {
-      throw new Error('no router configured for network ' + currentNetwork.chainId)
-    }
-    
+  async price(token: UniswapRouterV2Alias) {
+    const { route, routerAddress, outputDecimals } = token
     const router = uniswap.router.contract(this.provider, routerAddress);
 
-    const decimals = await erc20
+    const inputDecimals = await erc20
       .contract(this.provider, route[0])
       .decimals();
 
-    const [, priceInStablecoin] = await router.getAmountsOut(
-      ethereum.toBN(`1e${decimals}`),
+    const pairsPathConversionArray = await router.getAmountsOut(
+      ethereum.toBN(`1e${inputDecimals}`).toFixed(0),
       route
     );
 
-    return priceInStablecoin.div(`1e6`);
+    return pairsPathConversionArray[pairsPathConversionArray.length-1].div(`1e${outputDecimals}`);
   }
 }
 
@@ -166,7 +150,7 @@ export class CoingeckoProvider {
 }
 
 export type IdAlias = { id: string };
-export type UniswapRouterV2Alias = { route: string[] };
+export type UniswapRouterV2Alias = { route: string[]; routerAddress: string; outputDecimals: number };
 export type NetworkAlias = {
   network: number;
   address: string;
@@ -187,7 +171,7 @@ function isNetworkAlias(alias: Alias): alias is NetworkAlias {
 }
 
 function isUniswapRouterV2Alias(alias: Alias): alias is UniswapRouterV2Alias {
-  return Object.hasOwnProperty.call(alias, "network");
+  return Object.hasOwnProperty.call(alias, "route");
 }
 
 export function bridgeWrapperBuild(
@@ -223,7 +207,7 @@ export function bridgeWrapperBuild(
         }
 
         return new UniswapV2RouterProvider(provider)
-          .price(alias.route)
+          .price(alias)
       } else {
         return new CoingeckoProvider({
           block,
