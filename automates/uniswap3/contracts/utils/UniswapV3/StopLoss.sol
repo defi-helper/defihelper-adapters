00508d2d57dc1de316614abdc2d3c8c4362d5b6b
artur <amamedbekov@tokenomica.com>
2023-02-20 09:32:51 +0300

    stop loss fix

--- automates/uniswap3/contracts/utils/UniswapV3/StopLoss.sol
+++ automates/uniswap3/contracts/utils/UniswapV3/StopLoss.sol
@@ -20,8 +20,7 @@ library StopLoss {
   function run(
     Order storage order,
     address liquidityRouter,
-    address[] memory inTokens,
-    uint256 _deadline
+    address[] memory inTokens
   ) internal {
     require(order.path.length > 1 && order.amountOut > 0, "StopLoss::run: stop loss disabled");
     require(inTokens.length <= 256, "StopLoss::run: too many tokens");
@@ -31,37 +30,39 @@ library StopLoss {
       uint256 balance = IERC20(token).balanceOf(address(this));
       if (balance == 0) continue;
       IERC20(token).safeApprove(liquidityRouter, balance);
-      ISwapRouter(liquidityRouter).exactInput(ISwapRouter.ExactInputParams({
-        path: abi.encodePacked(token, order.fee, order.path[0]),
-        recipient: address(this),
-        deadline: _deadline,
-        amountIn: balance,
-        amountOutMinimum: 0
-      }));
+      ISwapRouter(liquidityRouter).exactInputSingle(
+        ISwapRouter.ExactInputSingleParams({
+          tokenIn: token,
+          tokenOut: order.path[0],
+          fee: order.fee,
+          recipient: address(this),
+          amountIn: balance,
+          amountOutMinimum: 0,
+          sqrtPriceLimitX96: 0
+        })
+      );
     }
 
     address baseToken = order.path[0];
     uint256 baseBalance = IERC20(baseToken).balanceOf(address(this));
-    uint256 amountOut;
+    uint256 amountOut = baseBalance;
     if (baseToken != order.path[order.path.length - 1]) {
       require(baseBalance > 0, "StopLoss::run: insufficient balance of base token");
       IERC20(baseToken).safeApprove(liquidityRouter, baseBalance);
       bytes memory pathBytes = abi.encodePacked(order.path[0]);
       for (uint256 i = 1; i < order.path.length; i++) {
-          pathBytes = bytes.concat(pathBytes, abi.encodePacked(uint24(order.fee), order.path[i]));
+        pathBytes = bytes.concat(pathBytes, abi.encodePacked(uint24(order.fee), order.path[i]));
       }
-      amountOut = ISwapRouter(liquidityRouter).exactInput(ISwapRouter.ExactInputParams({
+      amountOut = ISwapRouter(liquidityRouter).exactInput(
+        ISwapRouter.ExactInputParams({
           path: pathBytes,
           recipient: address(this),
-          deadline: _deadline,
           amountIn: baseBalance,
-          amountOutMinimum: order.amountOutMin 
-      }));
-      require(amountOut <= order.amountOut, "StopLoss::run: invalid output amount");
-    } else {
-      amountOut = baseBalance;
-      require(amountOut <= order.amountOut, "StopLoss::run: invalid output amount");
+          amountOutMinimum: order.amountOutMin
+        })
+      );
     }
+    require(amountOut <= order.amountOut, "StopLoss::run: invalid output amount");
     emit StopLossOrderCompleted(amountOut);
   }
 }
